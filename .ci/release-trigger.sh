0e8ad37396e9e0e5e535371c9c00446f313f110d
sebthom <sebthom@users.noreply.github.com>
2024-08-15 17:32:28 +0200

    ci: trigger release build

--- .ci/release-trigger.sh
+++ .ci/release-trigger.sh
@@ -1,7 +1,7 @@
 # this file is evaluated by .ci/build.sh
 
-POM_CURRENT_VERSION="1.0.4-SNAPSHOT" # perform release if pom.xml matches this version
-POM_RELEASE_VERSION="1.0.4" # next release version
+POM_CURRENT_VERSION="1.0.5-SNAPSHOT" # perform release if pom.xml matches this version
+POM_RELEASE_VERSION="1.0.5" # next release version
 
 DRY_RUN=false # is dry run?
 SKIP_TESTS=true # skip tests during release build?
