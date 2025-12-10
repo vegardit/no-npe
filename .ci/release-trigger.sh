0cea0a88dd82ae38dbeb1b7d9fe4726b2da28d15
sebthom <sebthom@users.noreply.github.com>
2025-12-10 13:15:20 +0100

    ci: trigger release build

--- .ci/release-trigger.sh
+++ .ci/release-trigger.sh
@@ -3,8 +3,8 @@
 
 # this file is evaluated by shared workflow https://github.com/sebthom/gha-shared/blob/v1/.github/workflows/maven-build.yml
 
-POM_CURRENT_VERSION="1.3.9-SNAPSHOT" # perform release if pom.xml matches this version
-POM_RELEASE_VERSION="1.3.9" # next release version
+POM_CURRENT_VERSION="1.3.10-SNAPSHOT" # perform release if pom.xml matches this version
+POM_RELEASE_VERSION="1.3.10" # next release version
 
 DRY_RUN=false # is dry run?
 SKIP_TESTS=true # skip tests during release build?
